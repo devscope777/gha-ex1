397357ea42534f500cc7d0b86612349de38433bc
devscope777 <devscope777@gmail.com>
2026-03-19 21:04:56 -0400

    New testing project

---
+++ src/main/java/com/example/gha_ex1/GhaEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.gha_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gha_ex1/GhaEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gha_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
